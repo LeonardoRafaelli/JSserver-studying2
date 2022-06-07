3cc57fb7c7edc32fa0d0a293876a37d7dfb21c18
Aluno <leonardorafaelli@gmail.com>
2022-06-07 08:16:52 -0300

    update

--- api/boletos.js
+++ api/boletos.js
@@ -2,6 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 
+// ID_Boleto
+// Valor
+// ID_User
+// ID_Pessoa
+// Status
+// Nome_Pessoa
+
+const billList = [
+    {
+        id: 1,
+        price: 500,
+        user_id: 1,
+        person_id: 1,
+        person_name: "Leo",
+        status: "Pending",
+    }
+]
+
+const fetchBills = () => {
+    return billList;
+}
+
+router.get('/', (req, res) => {
+    const billList = fetchBills();
+    res.json(billList);
+})
+
 
 module.exports = {
     router
